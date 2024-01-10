65250dc18c2c32eda9d80dde89d1497e711c54ae
Benature <wbenature@163.com>
2024-01-10 21:13:27 +0800

    update config and sync.py

--- src/sync.py
+++ src/sync.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from shutil import copytree
+from shutil import copytree, copyfile
 import yaml
+import os
 
 with open("./config.yaml", "r") as f:
     CONFIG = yaml.safe_load(f.read())
@@ -9,6 +10,16 @@ ob_path = Path(CONFIG['obsidian_base_path'])
 
 root_path = Path(__file__).resolve().parent.parent
 
-relative_paths = [".obsidian/snippets", "setting/js", "setting/templates"]
+relative_paths = [
+    ".obsidian/snippets", "setting/js", "setting/templates",
+    "setting/podcast.py"
+]
 for rp in relative_paths:
-    copytree(ob_path / rp, root_path / rp, dirs_exist_ok=True)
+    origin_p = ob_path / rp
+    print(origin_p)
+    if origin_p.is_dir():
+        copytree(origin_p, root_path / rp, dirs_exist_ok=True)
+    elif origin_p.is_file():
+        copyfile(origin_p, root_path / rp)
+    else:
+        raise Warning(f"Unknown path: {rp}")
